f3d7a105d91f2e85519a3caf746557208a9dd7fa
Michael Cheng <mcheng.work@gmail.com>
2025-03-31 13:20:56 +0800

    feat(ESG-12): Function to update the playlist

--- packages/sync-db/src/lib/services/esg_data.ts
+++ packages/sync-db/src/lib/services/esg_data.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import supabase from './supabase';
 import { toSlug } from '../helpers/url_helpers';
-import { ESGPlaylistDetails } from '../types';
+import { ESGPlaylistDetails, ESGVideoItem } from '../types';
 
 const breakIntoRanges = (number: number, rangeSize = 50) => {
   if (typeof number !== 'number' || number < 0 || !Number.isInteger(number)) {
@@ -72,4 +72,102 @@ export const fetchPlaylists = async (): Promise<ESGPlaylistDetails[]> => {
   }
 
   return videos;
+};
+
+export const updatePlaylist = async (
+  playlistId: string,
+  playlistDetails: ESGPlaylistDetails,
+  playlistItems?: ESGVideoItem[]
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): Promise<any> => {
+  if (!playlistId || !playlistDetails) {
+    throw new Error('Playlist ID and details are required.');
+  }
+
+  const { data: playlistDetailsRecord, error: updatePlayListError } =
+    await supabase.from('playlists').upsert(
+      {
+        playlistId: playlistId,
+        name: playlistDetails.playlistTitle,
+        description: playlistDetails.playlistDescription,
+        publish_date: playlistDetails.publishedAt,
+        image: playlistDetails.thumbnailDefault,
+        website: playlistDetails.website,
+        active: true,
+      },
+      { onConflict: 'playlist_id' }
+    );
+
+  if (updatePlayListError) {
+    console.error('Error updating playlist in Supabase:', updatePlayListError);
+    throw updatePlayListError;
+  }
+
+  if (!playlistDetailsRecord || !playlistItems || playlistItems.length === 0) {
+    return;
+  }
+
+  const { data: playlistItemsRecord, error: upsertError } = await supabase
+    .from('episodes')
+    .upsert(
+      playlistItems.map((item) => ({
+        video_id: item.videoId,
+        title: item.videoTitle,
+        publish_at: item.publishedAt,
+        description: item.videoDescription,
+        image1: item.thumbnailDefault,
+        image2: item.thumbnailMedium,
+        image3: item.thumbnailHigh,
+        active: true,
+        video_site: 1,
+      })),
+      { onConflict: 'video_id' }
+    )
+    .select();
+  if (upsertError) {
+    console.error('Error upserting playlist items in Supabase:', upsertError);
+    throw upsertError;
+  }
+
+  // Check if playlistItemsRecord is not empty
+  if (!playlistItemsRecord) {
+    console.error('No playlist items found in Supabase.');
+    return;
+  }
+
+  // Delete all associated playlist_items
+  const { error: deleteError } = await supabase
+    .from('playlist_items')
+    .delete()
+    .eq('playlist_id', playlistDetailsRecord['data'][0]['id']);
+
+  if (deleteError) {
+    console.error('Error deleting playlist items in Supabase:', deleteError);
+    throw deleteError;
+  }
+
+  // Insert new playlist_items
+  const { data: playlistItemsJoinTableRecord, error: insertError } =
+    await supabase
+      .from('playlist_items')
+      .insert(
+        playlistItemsRecord.map((item) => ({
+          playlist_id: playlistDetailsRecord['data'][0]['id'],
+          episode_id: item['id'],
+        }))
+      )
+      .select();
+  if (insertError) {
+    console.error('Error inserting playlist items in Supabase:', insertError);
+    throw insertError;
+  }
+  console.log(
+    `Inserted ${playlistItemsRecord.length} playlist items for playlist ID ${playlistId}`
+  );
+
+  return {
+    playlistDetailsRecord,
+    playlistItemsRecord,
+    playlistItemsJoinTableRecord,
+  };
 };
