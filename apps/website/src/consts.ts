adf5814c9ceb8f7a521e9fb38bb5ef09cab976be
Michael Cheng <michael_cheng@tech.gov.sg>
2025-04-27 16:28:38 +0800

    fix: Issue is misconfigured environment
    - Was making changes to Production env when i should be making changes to the Preview env in Cloudflare Pages.

--- apps/website/src/consts.ts
+++ apps/website/src/consts.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-console.log('Checking for env var', import.meta.env.SUPABASE_URL)
 export const SITE_TITLE = "Engineers.SG";
 export const SITE_DESCRIPTION = "Welcome to my website!";
--- packages/esg-data/src/lib/services/supabase.ts
+++ packages/esg-data/src/lib/services/supabase.ts
@@ -1,12 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config';
 
-console.log('What is in the env', process.env);
-
 const { SUPABASE_URL, SUPABASE_KEY } = process.env;
-
-console.log('SUPABASE_URL', SUPABASE_URL);
-console.log('SUPABASE_KEY', SUPABASE_KEY);
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
