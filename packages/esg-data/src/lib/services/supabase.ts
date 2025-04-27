41bd3c13a163caee368bb6703bb81b3d925e93bc
Michael Cheng <michael_cheng@tech.gov.sg>
2025-04-27 15:26:32 +0800

    bug: Find out what is retrieved

--- packages/esg-data/src/lib/services/supabase.ts
+++ packages/esg-data/src/lib/services/supabase.ts
@@ -3,6 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 
 const { SUPABASE_URL, SUPABASE_KEY } = process.env;
 
+console.log('SUPABASE_URL', SUPABASE_URL);
+console.log('SUPABASE_KEY', SUPABASE_KEY);
+
 // Create a single supabase client for interacting with your database
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
